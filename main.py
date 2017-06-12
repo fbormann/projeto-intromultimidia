acaa4275a2dcc54484a8a9356e4e57a48e257fb9
Felipe Bormann <felipe.bormann@gmail.com>
2017-06-12 17:24:06 -0300

    added new samples

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ vision_client = vision.Client('plenary-network-146618')
 # The name of the image file to annotate
 file_name = os.path.join(
     os.path.dirname(__file__),
-    'images/image001.jpg')
+    'images/image007.jpg')
 
 # Loads the image into memory
 with io.open(file_name, 'rb') as image_file:
@@ -26,6 +26,11 @@ print('Labels:')
 # Performs label detection on the image file
 labels = image.detect_labels()
 
+columns = ['muscle', 'arm', 'leg', 'abdomen', 'fitness', 'thigh']
+x = []
 for label in labels:
     print(label.description)
+    if label.description in columns:
+    	x.append(label.score)
     print(label.score)
+print(x)
